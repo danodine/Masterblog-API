64bb4d5d64a27fdc54c475b5af42634aafc32c5f
David Nodine <davidnodine@gmail.com>
2025-05-09 15:44:06 +0200

    Delete post

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -14,13 +14,27 @@ POSTS = [
 def get_posts():
     if request.method == 'POST':
         data = request.get_json()
-        if not  data.get('title') or not data.get('content'):
-            return "Post must have a title and content", 404,
+        if not data.get('title') or not data.get('content'):
+            return jsonify({"error": "Post must have a title and content"}), 400
+
         title = data.get('title')
         content = data.get('content')
         new_id = POSTS[-1]["id"] + 1
         POSTS.append({'id': new_id, 'title': title, 'content': content})
-    return jsonify(POSTS)
+
+    return jsonify(POSTS), 201
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_posts(post_id):
+    global POSTS
+    original_length = len(POSTS)
+    POSTS = [post for post in POSTS if post['id'] != post_id]
+
+    if len(POSTS) == original_length:
+        return jsonify({"error": f"Post with id {post_id} not found."}), 404
+
+    return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 201
 
 
 @app.errorhandler(404)
